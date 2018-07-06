fixed calculation bug in FOEIR, new evaluation results

runBenchmarking.py
@@ -75,7 +75,6 @@ def main():
             results += listResults
             fileNames += listFileNames
     
-    
     #read all data sets in German Credit
     for dirpath, dirnames, files in os.walk("scoredDataSets/GermanCredit/"):
         for name in files:
@@ -97,7 +96,7 @@ def main():
     df = pd.DataFrame(np.array(finalResults).reshape(len(finalResults),4), columns = ['Data_Set_Name', 'Algorithm_Name', 'Measure', 'Value'])
     
     df.to_csv('results/evaluationResults.csv', index=(False))
-     
+    
     plotData()
    
     endTime = datetime.datetime.now()
@@ -240,21 +239,10 @@ def scoreBasedEval(dataSetName, dataSetPath, k = 100, features = True, protected
     #creates a csv with candidates ranked with color-blind ranking
     createRankingCSV(originalRanking, 'Color-Blind/' + dataSetName + 'ranking.csv',k )
     #run the metrics ones for the color-blind ranking
-    evalResults += (runMetrics(evalK, protected, nonProtected, originalRanking, originalRanking, dataSetName, 'Color-Blind'))
-    
-    
-    #create ranking like Feldman et al.
-    feldRanking, pathFeldman = feldmanRanking(protected, nonProtected, k, dataSetName)
-    #Update the currentIndex of a candidate according to feldmanRanking
-    feldRanking = updateCurrentIndex(feldRanking)
-    #create CSV with rankings from FAIR
-    createRankingCSV(feldRanking, pathFeldman,k)
-    #evaluate FAIR with all available measures
-    evalResults += (runMetrics(evalK, protected, nonProtected, feldRanking, originalRanking, dataSetName, 'FeldmanEtAl'))
-    
+    evalResults += (runMetrics(evalK, protected, nonProtected, originalRanking, originalRanking, dataSetName, 'Color-Blind')) 
     
     #run evaluations for FOEIR with different Fairness Constraints
-    #we only produce rankings of k = 50 since construction of P as well as dicomposition of Birkhoff take a very long time
+    #we only produce rankings of k = 40 since construction of P as well as dicomposition of Birkhoff take a very long time
     #and consume a lot of memory.
     #run for FOEIR-DPC
     dpcRanking, dpcPath, isDPC = runFOEIR(originalRanking, dataSetName, 'FOEIR-DPC', evalK)
@@ -284,6 +272,15 @@ def scoreBasedEval(dataSetName, dataSetPath, k = 100, features = True, protected
     createRankingCSV(FAIRRanking, pathFAIR,k)
     #evaluate FAIR with all available measures
     evalResults += (runMetrics(evalK, protected, nonProtected, FAIRRanking, originalRanking, dataSetName, 'FAIR'))
+    
+    #create ranking like Feldman et al.
+    feldRanking, pathFeldman = feldmanRanking(protected, nonProtected, k, dataSetName)
+    #Update the currentIndex of a candidate according to feldmanRanking
+    feldRanking = updateCurrentIndex(feldRanking)
+    #create CSV with rankings from FAIR
+    createRankingCSV(feldRanking, pathFeldman,k)
+    #evaluate FAIR with all available measures
+    evalResults += (runMetrics(evalK, protected, nonProtected, feldRanking, originalRanking, dataSetName, 'FeldmanEtAl'))
     
     if features:
         try:

src/algorithms/ListNet/listnet.py
@@ -57,7 +57,7 @@ class ListNet(object):
                     dt = data[data.session == t]
                     if dt.score.sum() == 0:
                         continue
-                    xt = dt.loc[:,np.in1d(dt.columns,["session","score","sensitive_attribute",0],invert=True)]
+                    xt = dt.loc[:,np.in1d(dt.columns,["session","score","sensitive_attribute"],invert=True)]
                     self.X.append(xt.values.astype(np.float32))
                     self.Y.append(dt.score.values.astype(np.float32))
                 self.T = len(self.X)

src/candidateCreator/createCandidate.py
@@ -58,9 +58,9 @@ class createCandidate():
                 features = np.asarray(row[4:])
                 # access second row of .csv with protected attribute 0 = nonprotected group and 1 = protected group
                 if row[1] == 0:
-                    nonProtected.append(Candidate(1 - row[3]/l, 1- row[3]/l, [], row[3], row[2], features))
+                    nonProtected.append(Candidate(1 - float(row[3])/l, 1- float(row[3])/l, [], float(row[3]), row[2], features))
                 else:
-                    protected.append(Candidate(1 - row[3]/l, 1 - row[3]/l, "protectedGroup", row[3], row[2], features))
+                    protected.append(Candidate(1 - float(row[3])/l, 1 - float(row[3])/l, "protectedGroup", float(row[3]), row[2], features))
     
             queryRanking = nonProtected + protected
         
@@ -101,9 +101,9 @@ class createCandidate():
                     features = np.asarray(row[:(-2)])
                     # access second row of .csv with protected attribute 0 = nonprotected group and 1 = protected group
                     if row[-1] == 0:
-                        nonProtected.append(Candidate(row[-2], row[-2], [], i, [], features))
+                        nonProtected.append(Candidate(float(row[-2]), float(row[-2]), [], i, [], features))
                     else:
-                        protected.append(Candidate(row[-2], row[-2], "protectedGroup", i, [], features))
+                        protected.append(Candidate(float(row[-2]), float(row[-2]), "protectedGroup", i, [], features))
         except FileNotFoundError:
             raise FileNotFoundError("File could not be found. Something must have gone wrong during preprocessing.")                
     

src/measures/calculateDTRandDIR.py
@@ -29,26 +29,8 @@ def calculatedTRandDIR(ranking, algoName, dataSetName, k = 40):
     if k > 40:
         k = 40
         print('Calculation of P for k larger than 40 will not yield any results but just crash the program. Therefore k will be set to 40.')
-    
-    try:
-        if algoName == 'FOEIR-DIC':
-            
-            filepath = 'doublyStochasticPropMatrix/FOEIR-DIC/' + dataSetName + str(k) + '.csv'
-            x = readPFromFile(filepath, algoName)
-        elif algoName == 'FOEIR-DPC':
-            
-            filepath = 'doublyStochasticPropMatrix/FOEIR-DPC/' + dataSetName + str(k) + '.csv'
-            x = readPFromFile(filepath, algoName)
-            
-        elif algoName == 'FOEIR-DTC':
-            
-            filepath = 'doublyStochasticPropMatrix/FOEIR-DTC/' + dataSetName + str(k) + '.csv'
-            x = readPFromFile(filepath, algoName)
-        else:    
-            x = solveLPWithoutFairness(ranking, algoName, k)
-    except FileNotFoundError:
-        x = solveLPWithoutFairness(ranking, algoName, k)
-        pass
+       
+    x = solveLPWithoutFairness(ranking, algoName, k)
     
     x = np.reshape(x,(k,k))
     
@@ -301,26 +283,3 @@ def solveLPWithoutFairness(ranking,algoName, k):
     print('Finished solving LP without Fairness Constraints.')
     
     return np.array(sol['x'])
-
-def readPFromFile(filepath, algoName):
-    
-    """
-    Read earlier computed doubly stochastic matrix from file for FOEIR
-    
-    @param filepath: Path of the doubly stochastic matrix 
-    @param algoName: Name of the algorithm the doubly stochastiv matrix should be read for
-    
-    return the flattened doubly stochastic matrix
-    """
-    
-    #try to open csv file and save content in numpy array, if not found raise error
-    try:
-        with open(filepath, newline='') as File:  
-            reader = csv.reader(File)
-            x = np.array([row for row in reader])
-    except FileNotFoundError:
-        raise FileNotFoundError("Could not find file with P for" + algoName + '.')
-        
-    x = x.flatten()
-    
-    return x

src/visualizer/visualizeData.py
@@ -8,6 +8,7 @@ Created on Mon Jun  4 21:24:57 2018
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import pandas as pd
+import numpy as np
 
 #constants for algorithms
 COLORBLIND = 'Color-Blind'
@@ -78,12 +79,12 @@ def plotData():
             c = c[(c.Measure != 'DTR')]
             c = c[(c.Measure != 'FairnessAtK')]
             
-            ax = c.plot.bar(x= c.Measure, y=algoList)
+            ax = c.plot.bar(x= np.array(c.Measure), y=algoList)
             plt.ylabel('Relevance')
             
         else:
             
-            ax = c.plot.bar(x= c.Measure, y=algoList)
+            ax = c.plot.bar(x= np.array(c.Measure), y=algoList)
             plt.ylabel('Normalized Winning Number')
         
         axes = plt.gca()